428b0b839415d179edc56dd5e45654f55ba0a680
Isaac Nyaabila <nyaabilai@gmail.com>
2024-06-27 07:36:46 +0000

    sitting 26-06-2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(
+    private router: Router) {
+  }
   title = 'PORTFOLIO_MINE';
+
+  navig_resume() {
+    this.router.navigate(['/resume']).
+      then(() => {
+        window.location.reload();
+      });
+  }
+
+  navig_projects() {
+    this.router.navigate(['/projects']).
+      then(() => {
+        window.location.reload();
+      });
+  }
+
+  navig_contact() {
+    this.router.navigate(['/contact']).
+      then(() => {
+        window.location.reload();
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
